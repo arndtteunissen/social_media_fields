2bc679684f3c41b7da797f62d225b4ed59ec17da
Joshua Westerheide <account@jdoubleu.de>
2017-11-23 09:48:17 +0100

    Initialize TYPO3 package

---
+++ ext_emconf.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Copyright notice
+ *
+ * (c) 2017 arndtteunissen <dev@arndtteunissen.de>
+ * All rights reserved
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Social Media Fields',
+    'description' => 'Adds social media features to TYPO3 pages.',
+    'category' => 'fe',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.1.0-7.1.99',
+            'typo3' => '7.6.99-8.7.99'
+        ]
+    ],
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => false,
+    'author' => 'Arndtteunissen',
+    'author_email' => 'dev@arndtteunissen.de',
+    'author_company' => 'Arndtteunissen',
+];
