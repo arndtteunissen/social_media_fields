9e0ff6874f64c417ec122a95738ec9a9fdfa342a
Tim Schreiner <schreiner.tim@gmail.com>
2017-11-23 12:59:01 +0100

    Implement rendering classes

---
+++ Classes/Rendering/TagRenderer.php
@@ -0,0 +1,98 @@
+<?php
+namespace Arndtteunissen\SocialMediaFields\Rendering;
+
+/**
+ * Copyright notice
+ *
+ * (c) 2017 arndtteunissen <dev@arndtteunissen.de>
+ * All rights reserved
+ */
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\TagBuilder;
+
+/**
+ * Class TagRenderer
+ */
+class TagRenderer
+{
+    /**
+     * Tag builder instance
+     *
+     * @var TagBuilder
+     */
+    protected $tag = null;
+
+    /**
+     * name of the tag to be created by this view helper
+     *
+     * @var string
+     */
+    protected $tagName = '';
+
+    /**
+     * Names of all registered tag attributes
+     *
+     * @var array
+     */
+    private $tagAttributes = [];
+
+    /**
+     * @param TagBuilder $tag
+     * @return void
+     */
+    public function injectTag(TagBuilder $tag)
+    {
+        $this->tag = $tag;
+    }
+
+    /**
+     * Initializes the class.
+     *
+     * @return void
+     */
+    public function initialize()
+    {
+        $this->tag->reset();
+    }
+
+    /**
+     * @param string $tagName
+     */
+    public function setTagName($tagName)
+    {
+        $this->tagName = $tagName;
+    }
+
+    /**
+     * @param array $tagAttributes
+     */
+    public function setTagAttributes(array $tagAttributes)
+    {
+        $this->tagAttributes = $tagAttributes;
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     * @return void
+     */
+    public function addAttribute($name, $value)
+    {
+        $this->tagAttributes[$name] = $value;
+    }
+
+    /**
+     * Renders the whole tag.
+     *
+     * @param string $content
+     * @return string
+     */
+    public function render($content = '')
+    {
+        $this->tag->setTagName($this->tagName);
+        $this->tag->setContent($content);
+        $this->tag->addAttributes($this->tagAttributes);
+
+        return $this->tag->render();
+    }
+}
---
+++ Classes/Rendering/TypeRenderer.php
@@ -0,0 +1,81 @@
+<?php
+namespace Arndtteunissen\SocialMediaFields\Rendering;
+
+/**
+ * Copyright notice
+ *
+ * (c) 2017 arndtteunissen <dev@arndtteunissen.de>
+ * All rights reserved
+ */
+
+use Arndtteunissen\SocialMediaFields\Types\AbstractType;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class TypeRenderer
+ */
+class TypeRenderer
+{
+    /**
+     * @return array
+     */
+    public function render(): array
+    {
+        $tags       = [];
+        $renderType = $this->esimateTypeRenderer($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['social_media_fields']['types']);
+
+        if ($renderType !== null) {
+            $tags = $this->renderType($renderType);
+        }
+
+        return $tags;
+    }
+
+    /**
+     * @param AbstractType $type
+     * @return array
+     */
+    protected function renderType(AbstractType $type)
+    {
+        $tags = [];
+
+        $type->initialize();
+
+        if ($type->shouldRender()) {
+            $type->generate();
+            $tags = $type->render();
+        }
+
+        return $tags;
+    }
+
+    /**
+     * @param array $types
+     * @return AbstractType|null
+     */
+    protected function esimateTypeRenderer(array $types)
+    {
+        $sortedTypes = [];
+        $renderType  = null;
+
+        foreach ($types as $className) {
+            if (class_exists($className) && is_subclass_of($className, AbstractType::class)) {
+                /** @var AbstractType $type */
+                $type                              = GeneralUtility::makeInstance($className);
+                $sortedTypes[$type->getPriority()] = $type;
+            }
+        }
+
+        krsort($sortedTypes);
+
+        /** @var AbstractType $type */
+        foreach ($sortedTypes as $type) {
+            if ($type->shouldRender()) {
+                $renderType = $type;
+                break;
+            }
+        }
+
+        return $renderType;
+    }
+}
