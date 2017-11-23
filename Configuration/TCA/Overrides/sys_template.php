ef16058c25faaecd082ea3ff51104f00c2f2ecce
Tim Schreiner <schreiner.tim@gmail.com>
2017-11-23 12:04:37 +0100

    Add extension typoscript

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Copyright notice
+ *
+ * (c) 2017 arndtteunissen <dev@arndtteunissen.de>
+ * All rights reserved
+ */
+
+defined('TYPO3_MODE') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('social_media_fields', 'Configuration/TypoScript', 'Social Media Fields');
